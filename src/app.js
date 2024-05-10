17c78823f2e616de86f848309b6f57b4136a8c6e
jonfraser <61217893+jonRCX@users.noreply.github.com>
2024-05-10 17:18:20 +1000

    added expressjs

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import expressLayouts from 'express-ejs-layouts';
+
+const app = express();
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config();
+}
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src', 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
